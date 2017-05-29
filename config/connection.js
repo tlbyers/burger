f6e921e18b7417083d864e928fbd1f195e324a85
Terri Byers <Terri Byers>
2017-05-29 11:46:45 -0400

    add JawsDB

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if(process.env.JAWSDB_URL){
   connection=mysql.createConnection({ 
     host: 'localhost',
     user: 'root',
-    password: 'Lvc2344!6962',
+    password: '',
     database: 'burgers_db'
   });
 };
